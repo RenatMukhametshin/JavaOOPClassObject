dcb019360b6873ab6495a5f194ebc9491138ce25
RenatOnDemoStand <myemail@mydomain.com>
2021-07-20 11:28:13 +0500

    added pee() and getFoodEaten() methods, added foodEaten field

--- src/prj01Cat/Cat.java
+++ src/prj01Cat/Cat.java
@@ -8,13 +8,15 @@ public class Cat
     private double minWeight;
     private double maxWeight;
 
+    private double foodEaten;
+
     public Cat()
     {
         weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
         minWeight = 1000.0;
         maxWeight = 9000.0;
-
+        foodEaten = 0.0;
     }
 
     public void meow()
@@ -26,12 +28,21 @@ public class Cat
     public void feed(Double amount)
     {
         weight = weight + amount;
+        foodEaten = foodEaten + amount;
     }
 
     public void drink(Double amount)
     {
         weight = weight + amount;
     }
+
+    public void pee()
+    {
+        weight = weight - 0.5; //had the toilet
+        System.out.println("mmm...the cat went to the toilet.");
+    }
+
+    public double getFoodEaten(){ return foodEaten; }
 
     public Double getWeight()
     {
--- src/prj01Cat/Loader.java
+++ src/prj01Cat/Loader.java
@@ -35,6 +35,18 @@ public class Loader
         }
         System.out.println("Cat3 " + cat3.getStatus() + " after " + countMeow + " meows.");
 
+        cat1.feed(150.0);
+        cat2.feed(120.0);
+        cat3.feed(110.0);
+
+        cat1.pee();
+        cat2.pee();
+        cat3.pee();
+
+        System.out.println("Cat 1 eaten: " +cat1.getFoodEaten());
+        System.out.println("Cat 2 eaten: " +cat2.getFoodEaten());
+        System.out.println("Cat 3 eaten: " +cat3.getFoodEaten());
+
         Cat cat = new Cat();
         System.out.println(cat.getStatus());
 
